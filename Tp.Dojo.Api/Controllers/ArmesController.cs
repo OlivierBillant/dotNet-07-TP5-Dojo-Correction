0625782454e6f5a311605f65d3dc109f083e0669
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-09-01 12:10:20 +0200

    ADD API Controller Armes

---
+++ Tp.Dojo.Api/Controllers/ArmesController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tp.Dojo.Api.Models;
+using TpDojo.Business;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Tp.Dojo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArmesController : ControllerBase
+    {
+        private readonly ArmeService armeService;
+
+        public ArmesController(ArmeService armeService)
+        {
+            this.armeService = armeService;
+        }
+        // GET: api/<ArmesController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return this.Ok(await this.armeService.GetArmesAsync());
+        }
+
+        // GET api/<ArmesController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return this.Ok(await this.armeService.GetArmeByIdAsync(id));
+        }
+
+        // POST api/<ArmesController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] ArmeViewModel armeViewModel)
+        {
+            var armeDto = ArmeViewModel.ToArmeDto(armeViewModel);
+            await this.armeService.AddArmeAsync(armeDto);
+            return this.Created("", armeViewModel);
+        }
+
+        // PUT api/<ArmesController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] ArmeViewModel armeViewModel)
+        {
+            if (id != armeViewModel.Id)
+            {
+                return this.BadRequest();
+            }
+            var armeDto = ArmeViewModel.ToArmeDto(armeViewModel);
+            await this.armeService.UpdateArmeAsync(armeDto);
+            return this.NoContent();
+        }
+
+
+        // DELETE api/<ArmesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.Ok(this.armeService.RemoveArmeAsync(id));
+        }
+    }
+}
---
+++ Tp.Dojo.Api/Models/ArmeViewModel.cs
@@ -0,0 +1,46 @@
+﻿namespace Tp.Dojo.Api.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using TpDojo.Business.Dto;
+    using TpDojo.Dal.Entities;
+
+    public class ArmeViewModel
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public int Degats { get; set; }
+
+        [Display(Name = "Image")]
+        public string? ImageUrl { get; set; }
+
+
+        public string Display => $"{this.Nom} ({this.Degats})";
+
+        public bool CanDelete { get; set; }
+
+        internal static ArmeViewModel? FromArmeDto(ArmeDto? arme)
+            => arme is null
+            ? null
+            : new ArmeViewModel
+            {
+                Id = arme.Id,
+                Nom = arme.Nom,
+                Degats = arme.Degats,
+                ImageUrl = arme.ImageUrl,
+                CanDelete = !arme.HasAssociatedSamourai
+            };
+
+        internal static List<ArmeViewModel> FromArmes(List<ArmeDto> armeDtos)
+            => armeDtos.Select(FromArmeDto).ToList();
+
+        internal static ArmeDto ToArmeDto(ArmeViewModel armeViewModel)
+            => new()
+            {
+                Id = armeViewModel.Id,
+                Nom = armeViewModel.Nom,
+                Degats = armeViewModel.Degats,
+                ImageUrl = armeViewModel.ImageUrl,
+            };
+
+    }
+}
